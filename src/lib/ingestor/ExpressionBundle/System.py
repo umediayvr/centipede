8396f70a03ffe9e3caac243e52857db9fe4f2f65
Paulo Nogueira <paulo.nogueira@umedia.eu>
2017-09-19 21:07:57 -0700

    added system expressions

---
+++ src/lib/ingestor/ExpressionBundle/System.py
@@ -0,0 +1,22 @@
+import os
+import tempfile
+from ..ExpressionEvaluator import ExpressionEvaluator
+
+class _System(object):
+    """
+    Basic system functions.
+    """
+
+    @staticmethod
+    def tmpdir():
+        """
+        Return a temporary directory path (under the OS temp location)
+        """
+        return tempfile.mkdtemp()
+
+
+# tmp dir
+ExpressionEvaluator.register(
+    'tmpdir',
+    _System.tmpdir
+)
