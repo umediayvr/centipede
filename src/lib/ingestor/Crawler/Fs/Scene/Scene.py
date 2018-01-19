84825793c8dac28fe344bf65ab9ca19c34b2c15b
Valerie Bernard <valerie.bernard@umedia.eu>
2018-01-18 16:34:33 -0800

    removed unused init

--- src/lib/ingestor/Crawler/Fs/Scene/Scene.py
+++ src/lib/ingestor/Crawler/Fs/Scene/Scene.py
@@ -5,12 +5,6 @@ class Scene(File):
     Abstracted scene path crawler.
     """
 
-    def __init__(self, *args, **kwargs):
-        """
-        Create a scene crawler.
-        """
-        super(Scene, self).__init__(*args, **kwargs)
-
     @classmethod
     def extensions(cls):
         """
