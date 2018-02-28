ab3631eb47f08de86175d9c1870ca48102ce511d
Valerie Bernard <valerie.bernard@umedia.eu>
2018-02-27 16:37:37 -0800

    Removed obsolete renderpublish task

--- src/lib/ingestor/Task/Shotgun/__init__.py
+++ src/lib/ingestor/Task/Shotgun/__init__.py
@@ -1,5 +1,4 @@
 from .SGPlatePublish import SGPlatePublish
 from .SGImageSeqPublish import SGImageSeqPublish
 from .SGTexturePublish import SGTexturePublish
-from .SGRenderPublish import SGRenderPublish
 from .SGPublish import SGPublish
