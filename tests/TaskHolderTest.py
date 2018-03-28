49d5d39c97ce32b0f8d8fef8f218ef01ade7ac4f
Valerie Bernard <valerie.bernard@umedia.eu>
2018-03-28 14:43:26 -0700

    updated task holder test to use cleanup

--- tests/TaskHolderTest.py
+++ tests/TaskHolderTest.py
@@ -1,11 +1,11 @@
 import os
 import unittest
-import glob
 from .BaseTestCase import BaseTestCase
 from ingestor.Crawler.Fs import Path
 from ingestor.TaskHolderLoader import JsonLoader
 from ingestor.TaskWrapper import TaskWrapper
 from ingestor.Task import Task
+from ingestor.Crawler.Fs.Image import Jpg
 
 class TaskHolderTest(BaseTestCase):
     """Test TaskHolder."""
@@ -21,20 +21,21 @@ class TaskHolderTest(BaseTestCase):
         crawlers = Path.createFromPath(BaseTestCase.dataDirectory()).glob()
         for taskHolder in taskHolderLoader.taskHolders():
             taskHolder.run(crawlers)
+        jpgCrawlers = Path.createFromPath(self.__jsonConfig).globFromParent(['jpg'])
+        self.assertEqual(len(jpgCrawlers), 1)
+        self.assertIsInstance(jpgCrawlers[0], Jpg)
+        # TODO: Assert you can't delete it
+        self.addCleanup(self.cleanup, jpgCrawlers[0])
 
-    @classmethod
-    def tearDownClass(cls):
+    def cleanup(self, crawler):
         """
         Remove the data that was copied.
         """
-        # find images to delete
-        pathCrawler = Path.createFromPath(cls.__jsonConfig)
-        dummyTask = Task.create('remove')
-        for crawler in pathCrawler.globFromParent(['jpg']):
-            dummyTask.add(crawler, crawler.var("filePath"))
+        removeTask = Task.create('remove')
+        removeTask.add(crawler, crawler.var("filePath"))
         wrapper = TaskWrapper.create('subprocess')
         wrapper.setOption('user', '$UMEDIA_VERSION_PUBLISHER_USER')
-        wrapper.run(dummyTask)
+        wrapper.run(removeTask)
 
 
 if __name__ == "__main__":
