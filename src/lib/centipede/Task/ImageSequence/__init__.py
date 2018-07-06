68e998ca9d22e3a63664e66c7432dfcb32583134
Paulo Nogueira <paulondc@gmail.com>
2018-07-05 21:01:38 -0700

    added gafferScene task

--- src/lib/centipede/Task/ImageSequence/__init__.py
+++ src/lib/centipede/Task/ImageSequence/__init__.py
@@ -2,3 +2,4 @@ from .FFmpeg import FFmpeg
 from .SequenceThumbnail import SequenceThumbnail
 from .NukeScript import NukeScript
 from .NukeTemplate import NukeTemplate
+from .GafferScene import GafferScene
